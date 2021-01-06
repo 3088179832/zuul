e1cd41f1c14b094b8b0f02d2965a5957ea92e104
3088179832 <3088179832@qq.com>
2021-01-06 16:43:28 +0800

    test

---
+++ src/test/java/com/mashibing/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashibing.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
